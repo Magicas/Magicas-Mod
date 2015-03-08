2c18507ce2e35b4a9fd55de38a169a12eacfdd56
Magicas <jamesc554544@gmail.com>
2015-03-08 13:40:35 +0000

    Ores Drop Their Essecnes

--- java/net/gammas/magicas/blocks/BlockEarthEssenceOre.java
+++ java/net/gammas/magicas/blocks/BlockEarthEssenceOre.java
@@ -20,7 +20,7 @@ public class BlockEarthEssenceOre extends Block
 	
     public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
     {
-        return MagicasItems.magicWandBlue;
+        return MagicasItems.earthEssence;
     }
 
     /**
