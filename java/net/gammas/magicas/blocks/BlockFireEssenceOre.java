62452c12c0eca68d19881f54dca39d5db3bff9cb
Magicas <carterjames554@gmail.com>
2015-03-10 13:47:23 +0000

    Update BlockFireEssenceOre.java

--- java/net/gammas/magicas/blocks/BlockFireEssenceOre.java
+++ java/net/gammas/magicas/blocks/BlockFireEssenceOre.java
@@ -21,23 +21,7 @@ public class BlockFireEssenceOre extends Block
 	
     public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
     {
-        return MagicasItems.fireEssence;
-    }
-
-    /**
-     * Returns the usual quantity dropped by the block plus a bonus of 1 to 'i' (inclusive).
-     */
-    public int quantityDroppedWithBonus(int p_149679_1_, Random p_149679_2_)
-    {
-        return this.quantityDropped(p_149679_2_) + p_149679_2_.nextInt(p_149679_1_ + 1);
-    }
-
-    /**
-     * Returns the quantity of items to drop on block destruction.
-     */
-    public int quantityDropped(Random p_149745_1_)
-    {
-        return 4 + p_149745_1_.nextInt(2);
+        return MagicasItems.fireEssenceChunk;
     }
 
     /**
